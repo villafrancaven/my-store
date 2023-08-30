51d46b22db764be73a3f6bb1c59a6f6fb415ed23
villafrancaven <villafrancareyvean@gmail.com>
2023-08-30 23:15:44 +0800

    Setup global styling and MUI installation

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+    fontSize: 14,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 600,
+  },
+});
+
+export default theme;
